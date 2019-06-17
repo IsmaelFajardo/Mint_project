158d5b0ca54d6dbef7b4f395373ee7974cdfffa9
IsmaelFajardo <ismael.fajardop@gmail.com>
2019-06-17 03:11:58 -0500

    SQL script
    Se actualiza el modelo de datos y se sube un script SQL para crear la base de datos y las tablas del modelo

--- sql/mint_database.sql
+++ sql/mint_database.sql
@@ -1,56 +1,87 @@
 -- Crear la base de datos
-CREATE DATABASE mint
+CREATE DATABASE IF NOT EXISTS mint
 USE mint
 
 -- Crear tabla "Cliente"
-CREATE TABLE cliente(
-    clienteid   INT NOT NULL AUTO_INCREMENT,
-    nombre      VARCHAR(255),
-    apellidop   VARCHAR(255),
-    apellidom   VARCHAR(255),
-    fecha_nac   DATE,
-    genero      VARCHAR(255),
-    email       VARCHAR(255),
-    telefono    VARCHAR(255),
-    pais        VARCHAR(255),
-    calle       VARCHAR(255),
-    num_ext     VARCHAR(255),
-    num_int     VARCHAR(255),
-    col         VARCHAR(255),
-    estado      VARCHAR(255),
-    cp          SMALLINT,
-    tarjeta     INT,
-    PRIMARY_KEY (clienteid)
+CREATE TABLE IF NOT EXISTS clientes 
+(
+    clientes_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nombre              VARCHAR(255),
+    apellidop           VARCHAR(255),
+    apellidom           VARCHAR(255),
+    fecha_nac           DATE,
+    genero              VARCHAR(255),
+    email               VARCHAR(255),
+    telefono            VARCHAR(255),
+    pais                VARCHAR(255),
+    calle               VARCHAR(255),
+    num_ext             VARCHAR(255),
+    num_int             VARCHAR(255),
+    col                 VARCHAR(255),
+    estado              VARCHAR(255),
+    cp                  SMALLINT,
+    tarjeta             INT
 )
 
 
--- Crear tabla "Cliente"
-CREATE TABLE pedido(
-
+-- Crear tabla "Pedido"
+CREATE TABLE IF NOT EXISTS pedidos 
+(
+    pedido_id           INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    estado              VARCHAR(255),
+    fecha               DATETIME,
+    FOREIGN KEY (cliente_id) REFERENCES clientes(cliente_id),
+    FOREIGN KEY (carrito_id) REFERENCES carritos(carrito_id)
 )
 
 
--- Crear tabla "Cliente"
-CREATE TABLE producto(
-
+-- Crear tabla "Producto"
+CREATE TABLE IF NOT EXISTS productos 
+(
+    producto_id         VARCHAR(255) NOT NULL PRIMARY KEY,
+    marca               VARCHAR(255),
+    modelo              VARCHAR(255),
+    talla               INT,
+    cantidad            INT,
+    nombre              VARCHAR(255),
+    precio              FLOAT,
+    --imagen            ,
+    FOREIGN KEY (subcategoria_id) REFERENCES subcategorias(subcategoria_id)
 )
 
 
--- Crear tabla "Cliente"
-CREATE TABLE subcategoria(
-
+-- Crear tabla "Subcategoria"
+CREATE TABLE IF NOT EXISTS subcategorias 
+(
+    subcategoria_id    INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nombre              VARCHAR(255)
+    --imagen      
 )
 
 
--- Crear tabla "Cliente"
-CREATE TABLE categoria(
-
+-- Crear tabla "Categoria"
+CREATE TABLE IF NOT EXISTS categorias 
+(
+    categoria_id        INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nombre              VARCHAR(255),
+    --imagen      , 
 )
 
 
--- Crear tabla "Cliente"
-CREATE TABLE carrito
+-- Crear tabla "Carrito"
+CREATE TABLE IF NOT EXISTS carritos 
+(
+    carrito_id          INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    total               FLOAT,
+    FOREIGN KEY (pedido_id) REFERENCES pedidos(pedido_id),
+    FOREIGN KEY (producto_id) REFERENCES productos(producto_id)
+)
 
 
--- Crear tabla "Cliente"
-CREATE TABLE categoria_subcategoria
+-- Crear tabla "Categoria_Subcategoria"
+CREATE TABLE IF NOT EXISTS categorias_subcategorias 
+(
+    categoria_subcategoria_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    FOREIGN KEY (categoria_id) REFERENCES categorias(categoria_id),
+    FOREIGN KEY (subcategoria_id) REFERENCES subcategorias(subcategoria_id)
+)
