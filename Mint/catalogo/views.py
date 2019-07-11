fb3bb6f5c265b4730a7710a464690446f6ace235
PerlaGCastillo <contactoperlagodinez@gmail.com>
2019-07-10 22:00:30 -0500

    prefinal

--- Mint/catalogo/views.py
+++ Mint/catalogo/views.py
@@ -20,29 +20,8 @@ def catalogo(request):
 
 def producto(request, idProducto):
     """ Vista para atender la petición de la url  producto chamarra hombre/ """
-    #identificador = productos.objects.get(id)
     p = productos.objects.get(pk=idProducto)
     return render(request, "catalogo/producto.html",
         {
         "producto": p
         })
-
-
-# def productos(request):
-#     """vista para atender la petición de la url catalogo/productos"""
-#     nombre = productos.objects.all()
-#     precio = productos.objects.all()
-#
-#     return render(request, "catalogo/catalogo.html",
-#         {
-#         "nombre": nombre,
-#         "precio": precio
-#         }
-#     )
-
-
-# if request.method == "POST":
-#     idProductos = request.POST["idProductos"]
-#     idProductos = int(x=idProductos)
-#     idProducto1 = request.POST["idProducto1"]
-#     idProducto2 = request.POST["idProducto2"]
